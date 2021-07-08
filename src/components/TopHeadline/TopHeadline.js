97b5f37173475f7132dd26351fc13e91fde0cb19
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-07-08 20:07:49 +0600

    completed the axios problems

--- src/components/TopHeadline/TopHeadline.js
+++ src/components/TopHeadline/TopHeadline.js
@@ -10,11 +10,12 @@ const TopHeadline = () => {
     //     fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=e39dd6a850984a609a617cee981df236")
     //     .then(res => res.json())
     //     .then(data => setArticles(data.articles))
-    // }, [])
-    useEffect(() =>{
-        axios("https://newsapi.org/v2/top-headlines?country=us&apiKey=e39dd6a850984a609a617cee981df236")
-        .then(data => setArticles(data.data.articles))
-    }, [])
+    // }, []);
+    useEffect(() => {
+        const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=e39dd6a850984a609a617cee981df236";
+        axios(url).then(data => setArticles(data.data.articles));
+        //npm install axios -die install kore nite hobe.
+    }, []);
     return (
         <div>
             <h1>News: {articles?.length}</h1>
